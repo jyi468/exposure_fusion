cb6305094aec7cbcd596ead81bde246c58da8b50
jyi <joshua.yi@amadeus.com>
2018-11-30 12:05:11 -0500

    Use numpy functions, refactor

--- fusion.py
+++ fusion.py
@@ -6,34 +6,42 @@ import scipy as sp
 from matplotlib import pyplot as plt
 
 
-def get_weights(img):
-    print ''
+def get_weights(img_stack):
+    # weights = np.empty([len(img_stack), img_stack[0].shape[0], img_stack[0].shape[1]]).astype(np.float32)
+    weights = []
+    for i, img in enumerate(img_stack):
+        # Convert image to have intensities between 0 and 1
+        img = np.float32(img) / 255
+        c = contrast(img, i)
+        s = saturation(img)
+        e = exposedness(img)
+        # weights[i, :, :] = c * s * e
+        weights.append(c * s * e)
+
+    # Normalize values
+    sum_w = np.sum(weights, axis=0)
+    weights = np.divide(weights, sum_w, where=sum_w != 0)
+
+    return weights
 
 
-def contrast(img, w_c=1):
+def contrast(img, i, w_c=1):
     """Contrast is determined by absolute value of laplacian"""
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return np.absolute(cv2.Laplacian(gray, cv2.CV_8U)) ** w_c
+    return np.absolute(cv2.Laplacian(gray, cv2.CV_32F)) ** w_c
 
 
 def saturation(img, w_s=1):
     """Saturation is standard deviation within the RGB Channels"""
-    return np.std(img, axis=2) ** w_s
+    return np.std(img, axis=2, dtype=np.float32) ** w_s
 
 
 def exposedness(img, w_e=1, sigma=0.2):
     """Well-exposedness is raw pixel intensities of a channel. We scale from 0 to 1
     we also apply this to each channel separately"""
 
-    # Scale channels to be between 0 and 1. Convert to floating point numbers
-    img = img / 255.0
-
-    channels = np.empty([img.shape[0], img.shape[1], 3])
-    for c in range(img.shape[2]):
-        channel = img[:, :, c]
-        channels[:, :, c] = np.exp((-channel - 0.5) ** 2 / (2 * sigma ** 2))
-
-    return channels[:, :, 0] * channels[:, :, 1] * channels[:, :, 2]
+    # Calculate values for each element and then multiply across channels
+    return np.prod(np.exp(-((img - 0.5) ** 2 / (2 * sigma ** 2))), axis=2, dtype=np.float32) ** w_e
 
 
 def create_pyramid(img):
@@ -41,22 +49,10 @@ def create_pyramid(img):
 
 
 def exposure_fusion(img_stack):
-    weights = np.empty([len(img_stack), img_stack[0].shape[0], img_stack[0].shape[1]]).astype(np.uint8)
-    # weights = []
-    for i, img in enumerate(img_stack):
-        c = contrast(img)
-        s = saturation(img)
-        e = exposedness(img)
-        weights[i, :, :] = c * s * e
-        # weights.append(c * s * e)
-
-    # Normalize values
-    weights = weights / sum(weights)
-
+    weights = get_weights(img_stack)
     img_stack = np.array(img_stack)
-    r = np.empty([img_stack[0].shape[0], img_stack[0].shape[1], img_stack.shape[3]])
+    r = np.empty([img_stack[0].shape[0], img_stack[0].shape[1], img_stack.shape[3]], dtype=np.uint8)
     for i in range(img_stack.shape[3]):
         r[:, :, i] = np.sum(img_stack[:, :, :, i] * weights, axis=0)
 
-    print 'done'
-    return r.astype(np.uint8)
+    return np.uint8(r)
